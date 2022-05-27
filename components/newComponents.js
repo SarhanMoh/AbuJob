2a99b96781330e17c90d2d2604ffc7914435de37
SarhanMoh <meedo.s@hotmail.com>
2022-05-27 14:53:20 +0300

    27-05-mohomh

--- components/newComponents.js
+++ components/newComponents.js
@@ -1,109 +1,315 @@
 import React from "react";
 
-
 const options = [
-    {
-      label: "רכב",
-  
-      value: "Cars",
-  
-      color: "#6495ED",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-  
-    {
-      label: "שיפוצים",
-  
-      value: "Renovations",
-  
-      color: "#007fff",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-  
-    {
-      label: "טיפול",
-  
-      value: "Treatment",
-  
-      color: "dodgerblue",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-  
-    {
-      label: "אמנות ומלאכת יד",
-  
-      value: "Arts",
-  
-      color: "#6495ED",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-    {
-      label: "קוסמטיקה",
-  
-      value: "cosmetics",
-  
-      color: "#007fff",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-    {
-      label: "תיקונים ומלאכות",
-  
-      value: "Repairs",
-  
-      color: "dodgerblue",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-    {
-      label: "חשמלאות",
-  
-      value: "Electricians",
-  
-      color: "#6495ED",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-    {
-      label: "הוראה",
-  
-      value: "Teaching",
-  
-      color: "#007fff",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-    {
-      label: "מוסיקה",
-  
-      value: "Music",
-  
-      color: "dodgerblue",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-    {
-      label: "שירותי מכלות",
-  
-      value: "Grocery",
-  
-      color: "#6495ED",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-    {
-      label: "טכנאים",
-  
-      value: "Technicians",
-  
-      color: "#007fff",
-  
-      icon: require("../app/assets/tempIcon.png"),
-    },
-  ];
-  
-  exports.options = options;
+  {
+    label: "רכב",
+
+    value: "Cars",
+
+    color: "#69d2e7",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+
+  {
+    label: "שיפוצים",
+
+    value: "Renovations",
+
+    color: "#a7dbd8",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+
+  {
+    label: "טיפול",
+
+    value: "Treatment",
+
+    color: "#c02942",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+
+  {
+    label: "אמנות ומלאכת יד",
+
+    value: "Arts",
+
+    color: "#e0e4cc",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+  {
+    label: "קוסמטיקה",
+
+    value: "cosmetics",
+
+    color: "#f38630",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+  {
+    label: "תיקונים ומלאכות",
+
+    value: "Repairs",
+
+    color: "#fa6900",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+  {
+    label: "חשמלאות",
+
+    value: "Electricians",
+
+    color: "#fe4365",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+  {
+    label: "הוראה",
+
+    value: "Teaching",
+
+    color: "#fc9d9a",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+  {
+    label: "מוסיקה",
+
+    value: "Music",
+
+    color: "#f9cdad",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+  {
+    label: "שירותי מכלות",
+
+    value: "Grocery",
+
+    color: "#c8c8a9",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+  {
+    label: "טכנאים",
+
+    value: "Technicians",
+
+    color: "#83af9b",
+
+    icon: require("../app/assets/tempIcon.png"),
+  },
+];
+const recently = [
+  {
+    name: "עמותה 1",
+
+    key: 100,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "טיפול",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 4,
+  },
+  {
+    name: "עמותה 2",
+
+    key: 99,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "רכב",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 4,
+  },
+  {
+    name: "עמותה 3",
+
+    key: 98,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "שיפוצים",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 5,
+  },
+  {
+    name: "עמותה 4",
+
+    key: 97,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "קןסמטיקה",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 2,
+  },
+  {
+    name: "עמותה 4",
+
+    key: 96,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "חשמלאות",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 1,
+  },
+  {
+    name: "עמותה 5",
+
+    key: 95,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "קןסמטיקה",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 5,
+  },
+  {
+    name: "עמותה 6",
+
+    key: 94,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "טיפול",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 4,
+  },
+  {
+    name: "עמותה 7",
+
+    key: 93,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "מוסיקה",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 2,
+  },
+  {
+    name: "עמותה 8",
+
+    key: 92,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "מוסיקה",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 4,
+  },
+  {
+    name: "עמותה 9",
+
+    key: 91,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "הוראה",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 1,
+  },
+  {
+    name: "עמותה 10",
+
+    key: 90,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "חשמלאות",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 5,
+  },
+  {
+    name: "עמותה 11",
+
+    key: 89,
+
+    profilePic: require("../app/assets/profileIcon.png"),
+
+    catagory: "חשמלאות",
+
+    city: "ירושלים",
+
+    address: "מקום במדינה",
+
+    phone: "050-000-0000",
+
+    rating: 4,
+  },
+];
+
+exports.options = options;
+exports.recently = recently;
